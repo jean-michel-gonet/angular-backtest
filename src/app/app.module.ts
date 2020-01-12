36106961c3ee0b7148e0c8a905ef92cba04abc52
jean-michel-gonet <jmgonet@yahoo.com>
2020-01-12 09:14:53 +0100

    Added the simplest chart, with only static data. WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { DisplayComponent } from './display/display.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -3,6 +3,8 @@ import { StockService } from '../services/stock/stock.service';
 import { Simulation } from '../model/core/simulation';
 import { SwissQuoteAccount } from '../model/account.swissquote';
 import { BuyAndHoldStrategy } from '../model/strategy.buy-and-hold';
+import { ChartDataSets } from 'chart.js';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-display',
@@ -11,10 +13,32 @@ import { BuyAndHoldStrategy } from '../model/strategy.buy-and-hold';
 })
 export class DisplayComponent implements OnInit {
   simulation: Simulation;
+  lineChartData: ChartDataSets[] = [
+    { data: [85, 72, 78, 75, 77, 75], label: 'Crude oil prices' },
+  ];
+
+  lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June'];
+
+  lineChartOptions = {
+    responsive: true,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,255,0,0.28)',
+    },
+  ];
+
+  lineChartLegend = true;
+  lineChartPlugins = [];
+  lineChartType = 'line';
+
   constructor(private stockService:StockService) {
   }
 
   ngOnInit() {
+    /*
     // Fetch the data:
     this.stockService.getStockData(['LU1290894820CHF4', 'CH0017810976CHF9']).subscribe(data => {
 
@@ -30,5 +54,6 @@ export class DisplayComponent implements OnInit {
       // Run the simulation:
       this.simulation.run(new Date(2010, 1, 1), new Date (2020, 1, 1));
     });
+    */
   }
 }
