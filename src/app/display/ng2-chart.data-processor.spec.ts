ac96805748584b5f1755ac708a49c4d157492bf6
jean-michel-gonet <jmgonet@yahoo.com>
2020-01-28 17:59:40 +0100

    Reporting the results to a graph. WIP

--- src/app/display/ng2-chart.data-processor.spec.ts
+++ src/app/display/ng2-chart.data-processor.spec.ts
@@ -1,4 +1,6 @@
 import { Ng2ChartDataProcessor, ShowDataAs, ShowDataOn } from "./ng2-chart.data-processor";
+import { ProvidedData } from '../model/core/data-processor';
+import { ChartDataSets } from 'chart.js';
 
 describe('GraphicData', () => {
   it('should create an instance', () => {
@@ -24,5 +26,90 @@ describe('GraphicData', () => {
             on: ShowDataOn.RIGHT
           }
         ])).toBeTruthy();
+  });
+
+  let today = new Date(2015, 8, 18);
+  let tomorrow = new Date(2015, 8, 19);
+
+  it('Can process to data the same set', () => {
+    let ng2ChartDataProcessor: Ng2ChartDataProcessor = new Ng2ChartDataProcessor([{
+      show: "SQA01.NAV",
+      as: ShowDataAs.LINE,
+      on: ShowDataOn.LEFT
+    }]);
+
+    ng2ChartDataProcessor.receiveData(new ProvidedData({
+      sourceName: "SQA01.NAV",
+      time: today,
+      y: 100
+    }));
+    ng2ChartDataProcessor.receiveData(new ProvidedData({
+      sourceName: "SQA01.NAV",
+      time: tomorrow,
+      y: 101
+    }));
+
+    expect(ng2ChartDataProcessor.dataSets).toEqual(jasmine.arrayWithExactContents([{
+      data: [100, 101],
+      label: "SQA01.NAV",
+      yAxisID: "y-axis-left",
+      type: "line"
+    }]));
+    expect(ng2ChartDataProcessor.labels).toEqual([
+      today.toDateString(),
+      tomorrow.toDateString()
+    ]);
+  });
+
+  it('Can process four data of two different sets', () => {
+    let ng2ChartDataProcessor: Ng2ChartDataProcessor = new Ng2ChartDataProcessor([
+      {
+        show: "SQA01.NAV",
+        as: ShowDataAs.LINE,
+        on: ShowDataOn.LEFT
+      }, {
+        show: "SQA01.COSTS",
+        as: ShowDataAs.BAR,
+        on: ShowDataOn.RIGHT
+      }]);
+
+    ng2ChartDataProcessor.receiveData(new ProvidedData({
+      sourceName: "SQA01.NAV",
+      time: today,
+      y: 100
+    }));
+    ng2ChartDataProcessor.receiveData(new ProvidedData({
+      sourceName: "SQA01.COSTS",
+      time: today,
+      y: 1
+    }));
+    ng2ChartDataProcessor.receiveData(new ProvidedData({
+      sourceName: "SQA01.NAV",
+      time: tomorrow,
+      y: 101
+    }));
+    ng2ChartDataProcessor.receiveData(new ProvidedData({
+      sourceName: "SQA01.COSTS",
+      time: tomorrow,
+      y: 2
+    }));
+
+    expect(ng2ChartDataProcessor.dataSets).toEqual(jasmine.arrayWithExactContents([
+      {
+        data: [100, 101],
+        label: "SQA01.NAV",
+        yAxisID: "y-axis-left",
+        type: "line"
+      }, {
+        data: [1, 2],
+        label: "SQA01.COSTS",
+        yAxisID: "y-axis-right",
+        type: "bar"
+      }
+    ]));
+    expect(ng2ChartDataProcessor.labels).toEqual([
+      today.toDateString(),
+      tomorrow.toDateString()
+    ]);
   });
 });
--- src/app/display/ng2-chart.data-processor.ts
+++ src/app/display/ng2-chart.data-processor.ts
@@ -13,7 +13,7 @@ export enum ShowDataOn {
 };
 
 export class Show {
-  show: String;
+  show: string;
   as: ShowDataAs;
   on: ShowDataOn;
 }
@@ -24,17 +24,86 @@ export class Show {
  * @class {Ng2ChartDataProcessor}
  */
 export class Ng2ChartDataProcessor implements DataProcessor {
-  public dataSets: ChartDataSets;
+  public dataSets: ChartDataSets[];
   public labels: Label[];
-  public options: ChartOptions;
+  public options: ChartOptions = {
+      legend: {
+        display: true
+      },
+      scales: {
+        yAxes: [
+          {
+            id: "y-axis-left",
+            position: 'left',
+            ticks: {
+              beginAtZero: true
+            }
+          }, {
+            id: "y-axis-right",
+            position: 'right',
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+        xAxes: [{
+          ticks: {
+            display: true,
+          }
+        }],
+      }
+    };
 
-  private show: Show[];
+  private mapOfDatasets: Map<String, ChartDataSets>;
+  private lastTime: Date;
 
   constructor(obj = [] as Show[]) {
-    this.show = obj;
+    this.mapOfDatasets = new Map<String, ChartDataSets>();
+    this.dataSets = [];
+    this.labels = [];
+    obj.forEach(show => {
+      let yAxisID:string = "y-axis-" + this.showOn(show.on);
+      let dataSet: ChartDataSets = {
+        data: [],
+        label: show.show,
+        yAxisID: yAxisID,
+        type: this.showAs(show.as)
+      };
+      this.mapOfDatasets.set(show.show, dataSet);
+      this.dataSets.push(dataSet);
+    });
   }
 
   receiveData(providedData: ProvidedData): void {
-    console.log(providedData);
+    let dataSet: ChartDataSets = this.mapOfDatasets.get(providedData.sourceName);
+    if (dataSet) {
+      dataSet.data.push(providedData.y);
+      if (!this.lastTime || this.lastTime.getDate() < providedData.time.getDate()) {
+        this.labels.push(providedData.time.toDateString());
+        this.lastTime = providedData.time;
+      }
+    }
+  }
+
+  private showOn(showOn: ShowDataOn):string {
+    switch(showOn) {
+      case ShowDataOn.LEFT:
+        return "left";
+      case ShowDataOn.RIGHT:
+        return "right";
+      default:
+        console.warn("showOn=" + showOn + ": unknown literal of ShowDataOn");
+        return "";
+    }
+  }
+  private showAs(showAs: ShowDataAs):string {
+    switch(showAs) {
+      case ShowDataAs.LINE:
+        return "line";
+      case ShowDataAs.BAR:
+        return "bar";
+      default:
+        console.warn("showAs=" + showAs + ": unknown literal of ShowDataAs");
+        return "";
+    }
   }
 }
