94ea07da1029982f9bf150aeae0bd66347135e9a
jean-michel-gonet <jmgonet@yahoo.com>
2020-02-08 19:25:11 +0100

    Concerns #3 - Preparing the Dividends - Still WIP

--- src/app/services/stock/stock.service.spec.ts
+++ src/app/services/stock/stock.service.spec.ts
@@ -1,11 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SixConnectionService } from './six-connection.service';
 import { YahooConnectionService } from './yahoo-connection.service';
 import { DateYieldConnectionService } from './date-yield-connection.service';
 import { StockService } from './stock.service';
 import { ConnectionService } from './connection.service';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { StockData, Dividend, Stock } from 'src/app/model/core/stock';
 import { QuoteSourceAndProvider, SecuritiesConfigurationService } from './securities-configuration.service';
 import { AssetOfInterest } from 'src/app/model/core/asset';
@@ -14,21 +14,24 @@ class ConnectionServiceMock implements ConnectionService {
   private stockData: Map<string, StockData> = new Map<string, StockData>();
   private dividends: Map<string, Dividend[]> = new Map<string, Dividend[]>();
 
-  whenQuotes(source: string, name: string, answer: StockData): void {
-    this.stockData.set(source + ":" + name, answer);
+  whenQuotes(name: string, answer: StockData): void {
+    this.stockData.set(name, answer);
   }
-  whenDividends(source: string, name: string, answer: Dividend[]): void {
-    this.dividends.set(source + ":" + name, answer);
+  whenDividends(name: string, answer: Dividend[]): void {
+    this.dividends.set(name, answer);
   }
 
   getQuotes(source: string, name: string): Observable<StockData> {
-    return new Observable<StockData>(observer =>{
-      observer.next(this.stockData.get(source + ":" + name))
+    let stockData: StockData = this.stockData.get(name);
+    return new Observable<StockData>(observer => {
+      observer.next(stockData);
+      observer.complete();
     });
   }
   getDividends(source: string, name: string): Observable<Dividend[]> {
     return new Observable<Dividend[]>(observer => {
-      observer.next(this.dividends.get(source + ":" + name));
+      observer.next(this.dividends.get(name));
+      observer.complete();
     });
   }
 }
@@ -80,7 +83,7 @@ describe('StockService', () => {
     expect(stockService).toBeTruthy();
   });
 
-  it('Can retrieve from Yahoo data', () => {
+  it('Can retrieve from Yahoo data', (done: DoneFn) => {
     configurationService.when("NAME", {
       name: "NAME",
       provider: "finance.yahoo.com",
@@ -95,48 +98,18 @@ describe('StockService', () => {
       new Stock({time: beforeYesterday, assetsOfInterest: [
         new AssetOfInterest({isin: "ISIN3", partValue: 1.3})
       ]})]);
-    six.whenQuotes("NAME", "xx", stockData);
+    yahoo.whenQuotes("NAME", stockData);
 
     let dividends: Dividend[] = [new Dividend(
       {isin: "NAME", dividend: 1.5, time: beforeYesterday}
     )];
 
-    dateYield.whenDividends("NAME", "yy", dividends);
+    dateYield.whenDividends("NAME", dividends);
 
-    return stockService.getStockData(["NAME"]).subscribe(data => {
-      expect(data.get(beforeYesterday).assetOfInterest("ISIN3").partValue).toBe(100);
+    stockService.getStockData(["NAME"]).subscribe(data => {
+      expect(data.get(beforeYesterday).assetOfInterest("ISIN3").partValue).toBe(1.3);
+      //expect(data.get(beforeYesterday).assetOfInterest("ISIN3").dividend).toBe(1.5);
+      done();
     });
   });
-
-/*
-it('Can retrieve from Yahoo data (2)', async(inject( [StockService], ( stockService: StockService ) => {
-  configurationService.when("NAME", {
-    name: "NAME",
-    provider: "finance.yahoo.com",
-    source: "xx",
-    dividends: {
-      provider: "date.yield.csv",
-      source: "yy",
-    }
-  });
-
-  let stockData: StockData = new StockData([
-    new Stock({time: beforeYesterday, assetsOfInterest: [
-      new AssetOfInterest({isin: "ISIN3", partValue: 1.3})
-    ]})]);
-  six.whenQuotes("NAME", "xx", stockData);
-
-  let dividends: Dividend[] = [new Dividend(
-    {isin: "NAME", dividend: 1.5, time: beforeYesterday}
-  )];
-
-  dateYield.whenDividends("NAME", "yy", dividends);
-
-  stockService.getStockData(["NAME"]).subscribe(data => {
-    expect(data.get(beforeYesterday).assetOfInterest("ISIN3").partValue).toBe(1.3);
-    expect(data.get(beforeYesterday).assetOfInterest("ISIN3").dividend).toBe(1.5);
-  });
-})));
-*/
-
 });
