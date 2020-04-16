32b194ba8a6bcf25e35f10c709029f780ba70803
jean-michel-gonet <jmgonet@yahoo.com>
2020-04-16 19:54:20 +0200

    Concerns #25 - Unit tested the MACD filter

--- src/app/model/markettiming/market-timing.macd.spec.ts
+++ src/app/model/markettiming/market-timing.macd.spec.ts
@@ -2,6 +2,7 @@ import { Quote } from '../core/quotes'
 import { BearBull } from '../core/market-timing';
 import { MACDMarketTiming } from './market-timing.macd';
 import { PeriodLength } from '../core/period';
+import { MovingAverageSource, MovingAveragePreprocessing } from '../core/moving-average';
 
 describe('MACDMarketTiming', () => {
   it('Can create a new instance', () => {
@@ -9,41 +10,83 @@ describe('MACDMarketTiming', () => {
     })).toBeTruthy();
   });
 
-  let makeMacd = function(start: Date, values:number[]): MACDMarketTiming {
-    let macd: MACDMarketTiming = new MACDMarketTiming({
+  /**
+   * To verify this test:
+   * - go to www.investing.com
+   * - look for SPY data.
+   * - Open interactive chart, and maximize it.
+   * - select "1w - Weekly" data
+   * - Navigate to zoom on a period between April and August 2019
+   * - Add Indicators -> MACD with 26, 12 and 9
+   * - Verify the values of the MACD and SIGNAL with your cursor.
+   */
+  it('Can calculate weekly MACD based on last closing price', () => {
+    let macdFilter: MACDMarketTiming = new MACDMarketTiming({
       id: "MACD",
-      periodLength: PeriodLength.DAILY,
+      source: MovingAverageSource.CLOSE,
+      preprocessing: MovingAveragePreprocessing.LAST,
+      periodLength: PeriodLength.WEEKLY,
       slowPeriod: 26,
       fastPeriod: 12,
       signalPeriod: 9,
-      status: BearBull.BEAR
+      status: BearBull.BULL
     });
 
-    for (let n: number = 0; n < values.length; n++){
-      let instant =  new Date(start.getFullYear(), start.getMonth(), n);
-      let quote = new Quote({name: "ISIN1", close: values[n]});
-      macd.record(instant, quote);
-    }
-    return macd;
-  }
+    macdFilter.fastEma.lastValue = 282.78;
+    macdFilter.slowEma.lastValue = 277.64;
+    macdFilter.signalEma.lastValue = 2.50;
 
-  it('Can follow a real case https://investsolver.com/calculate-macd-in-excel/ (1)', () => {
-    let macd = makeMacd(new Date(2014, 7, 0), [
-      58.66, 58.95, 59.88, 59.48, 59.16, 59.47, 58.80, 58.11, 57.83, 56.70,
-      57.08, 56.64]);
-    expect(Math.abs(macd.fastEma.lastValue - 58.21)).toBeLessThan(0.01);
-  });
+    macdFilter.record(new Date(2019, 4 - 1, 29), new Quote({name: 'SPY', close: 293.87}));
+    macdFilter.record(new Date(2019, 4 - 1, 30), new Quote({name: 'SPY', close: 294.02}));
+    macdFilter.record(new Date(2019, 5 - 1,  1), new Quote({name: 'SPY', close: 291.81}));
+    macdFilter.record(new Date(2019, 5 - 1,  2), new Quote({name: 'SPY', close: 291.18}));
+    macdFilter.record(new Date(2019, 5 - 1,  3), new Quote({name: 'SPY', close: 294.03}));
 
-  it('Can follow a real case https://investsolver.com/calculate-macd-in-excel/ (2)', () => {
-    let macd = makeMacd(new Date(2014, 7, 0), [
-      58.66, 58.95, 59.88, 59.48, 59.16, 59.47, 58.80, 58.11, 57.83, 56.70,
-      57.08, 56.64, 57.11, 56.73, 56.98, 57.48, 57.92, 57.46, 57.58, 57.60,
-      58.61, 57.79, 57.89, 58.58, 57.64, 58.81, 58.77, 60.21, 60.12, 59.76,
-      56.47, 57.84, 57.45, 58.04, 58.95, 60.09, 60.70, 61.77, 62.35, 62.59,
-      62.58, 62.36]);
+    macdFilter.record(new Date(2019, 5 - 1,  6), new Quote({name: 'SPY', close: 292.82}));
+    expect(macdFilter.macd)  .toBeCloseTo(5.66, 2);
+    expect(macdFilter.signal).toBeCloseTo(3.13, 2);
+    macdFilter.record(new Date(2019, 5 - 1,  7), new Quote({name: 'SPY', close: 287.93}));
+    macdFilter.record(new Date(2019, 5 - 1,  8), new Quote({name: 'SPY', close: 287.53}));
+    macdFilter.record(new Date(2019, 5 - 1,  9), new Quote({name: 'SPY', close: 286.66}));
+    macdFilter.record(new Date(2019, 5 - 1, 10), new Quote({name: 'SPY', close: 288.10}));
 
-      expect(macd.fastEma.lastValue).toBeCloseTo(60.61, 2);
-      expect(macd.slowEma.lastValue).toBeCloseTo(59.58, 2);
-      expect(macd.macd).toBeCloseTo(1.028, 3);
+    macdFilter.record(new Date(2019, 5 - 1, 13), new Quote({name: 'SPY', close: 280.86}));
+    expect(macdFilter.macd)  .toBeCloseTo(5.52, 2);
+    expect(macdFilter.signal).toBeCloseTo(3.61, 2);
+    macdFilter.record(new Date(2019, 5 - 1, 14), new Quote({name: 'SPY', close: 283.40}));
+    macdFilter.record(new Date(2019, 5 - 1, 15), new Quote({name: 'SPY', close: 285.06}));
+    macdFilter.record(new Date(2019, 5 - 1, 16), new Quote({name: 'SPY', close: 287.70}));
+    macdFilter.record(new Date(2019, 5 - 1, 17), new Quote({name: 'SPY', close: 285.84}));
+
+    macdFilter.record(new Date(2019, 5 - 1, 20), new Quote({name: 'SPY', close: 283.95}));
+    expect(macdFilter.macd)  .toBeCloseTo(5.18, 2);
+    expect(macdFilter.signal).toBeCloseTo(3.92, 2);
+    macdFilter.record(new Date(2019, 5 - 1, 21), new Quote({name: 'SPY', close: 286.51}));
+    macdFilter.record(new Date(2019, 5 - 1, 22), new Quote({name: 'SPY', close: 285.63}));
+    macdFilter.record(new Date(2019, 5 - 1, 23), new Quote({name: 'SPY', close: 282.14}));
+    macdFilter.record(new Date(2019, 5 - 1, 24), new Quote({name: 'SPY', close: 282.78}));
+
+    macdFilter.record(new Date(2019, 5 - 1, 28), new Quote({name: 'SPY', close: 280.15}));
+    expect(macdFilter.macd)  .toBeCloseTo(4.60, 2);
+    expect(macdFilter.signal).toBeCloseTo(4.06, 2);
+    macdFilter.record(new Date(2019, 5 - 1, 29), new Quote({name: 'SPY', close: 278.27}));
+    macdFilter.record(new Date(2019, 5 - 1, 30), new Quote({name: 'SPY', close: 279.03}));
+    macdFilter.record(new Date(2019, 5 - 1, 31), new Quote({name: 'SPY', close: 275.27}));
+
+    macdFilter.record(new Date(2019, 6 - 1,  3), new Quote({name: 'SPY', close: 274.57}));
+    expect(macdFilter.macd)  .toBeCloseTo(3.50, 2);
+    expect(macdFilter.signal).toBeCloseTo(3.95, 2);
+    macdFilter.record(new Date(2019, 6 - 1,  4), new Quote({name: 'SPY', close: 280.53}));
+    macdFilter.record(new Date(2019, 6 - 1,  5), new Quote({name: 'SPY', close: 282.96}));
+    macdFilter.record(new Date(2019, 6 - 1,  6), new Quote({name: 'SPY', close: 284.80}));
+    macdFilter.record(new Date(2019, 6 - 1,  7), new Quote({name: 'SPY', close: 287.65}));
+
+    macdFilter.record(new Date(2019, 6 - 1, 10), new Quote({name: 'SPY', close: 288.97}));
+    expect(macdFilter.macd)  .toBeCloseTo(3.58, 2);
+    expect(macdFilter.signal).toBeCloseTo(3.87, 2);
+    macdFilter.record(new Date(2019, 6 - 1, 11), new Quote({name: 'SPY', close: 288.90}));
+    macdFilter.record(new Date(2019, 6 - 1, 12), new Quote({name: 'SPY', close: 288.39}));
+    macdFilter.record(new Date(2019, 6 - 1, 13), new Quote({name: 'SPY', close: 289.58}));
+    macdFilter.record(new Date(2019, 6 - 1, 14), new Quote({name: 'SPY', close: 289.26}));
   });
 });
