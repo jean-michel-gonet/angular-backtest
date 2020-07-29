62d08a55f6fb3c8a295466549b8f35a623933393
jgonet <jgonet@pictet.com>
2020-07-29 07:47:08 +0200

    Stop Loss market timing uses linear regression to detect recovery

--- src/app/model/markettiming/market-timing.stop-loss.spec.ts
+++ src/app/model/markettiming/market-timing.stop-loss.spec.ts
@@ -31,7 +31,8 @@ describe("StopLosMarketTiming", () => {
     let stopLossMarketTiming: StopLossMarketTiming = new StopLossMarketTiming({
       status: BearBull.BULL,
       threshold: 0.95,
-      safety: 4
+      safety: 4,
+      recovery: 0.1
     });
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BULL);
     stopLossMarketTiming.record(new Date(), new Candlestick({close: 80}));
@@ -48,46 +49,51 @@ describe("StopLosMarketTiming", () => {
     let stopLossMarketTiming: StopLossMarketTiming = new StopLossMarketTiming({
       status: BearBull.BULL,
       threshold: 0.95,
-      safety: 2
+      safety: 2,
+      recovery: 0.1
     });
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 100}));
+    stopLossMarketTiming.record(new Date(2000, 1, 1), new Candlestick({close: 100}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BULL);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 94}));
+    stopLossMarketTiming.record(new Date(2000, 1, 2), new Candlestick({close: 94}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 95}));
+    stopLossMarketTiming.record(new Date(2000, 1, 3), new Candlestick({close: 95}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 96}));
+    stopLossMarketTiming.record(new Date(2000, 1, 4), new Candlestick({close: 96}));
+    expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
+
+    stopLossMarketTiming.record(new Date(2000, 1, 5), new Candlestick({close: 97}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BULL);
   });
 
-  it("Can react to a second drop", () => {
+  it("Can detect a second drop after a recovery", () => {
     let stopLossMarketTiming: StopLossMarketTiming = new StopLossMarketTiming({
       status: BearBull.BULL,
       threshold: 0.95,
-      safety: 2
+      safety: 2,
+      recovery: 0.1
     });
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 100}));
+    stopLossMarketTiming.record(new Date(2000, 1, 1), new Candlestick({close: 100}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BULL);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 44}));
+    stopLossMarketTiming.record(new Date(2000, 1, 2), new Candlestick({close: 94}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 45}));
+    stopLossMarketTiming.record(new Date(2000, 1, 3), new Candlestick({close: 95}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 44}));
+    stopLossMarketTiming.record(new Date(2000, 1, 4), new Candlestick({close: 96}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 45}));
-    expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
-
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 50}));
+    stopLossMarketTiming.record(new Date(2000, 1, 5), new Candlestick({close: 97}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BULL);
 
-    stopLossMarketTiming.record(new Date(), new Candlestick({close: 47.49}));
+    stopLossMarketTiming.record(new Date(2000, 1, 6), new Candlestick({close: 92.15}));
+    expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BULL);
+
+    stopLossMarketTiming.record(new Date(2000, 1, 6), new Candlestick({close: 92.14}));
     expect(stopLossMarketTiming.bearBull()).toBe(BearBull.BEAR);
   });
 
--- src/app/model/markettiming/market-timing.stop-loss.ts
+++ src/app/model/markettiming/market-timing.stop-loss.ts
@@ -1,34 +1,55 @@
 import { MarketTiming, BearBull } from '../core/market-timing';
 import { Candlestick } from '../core/quotes';
 import { Report } from '../core/reporting';
+import { OnlineLinearRegression } from '../calculations/linear-regression';
 
 interface IStopLossMarketTiming {
+  /** The idientifier of this market timing.*/
   id?: string,
+  /** The initial status.*/
   status?: BearBull;
+  /**
+   * The amount of the drop before activating stop loss.
+   * Specify 0.95 if you want to stop your loss after a drop of 5%.
+   */
   threshold?: number;
+  /**
+   * The number of days to wait before checking the linear regression for
+   * recovery. Calculation for linear regression starts immediately, though.
+   */
   safety?: number;
+  /**
+   * Minimal value for linear regression's A factor to recover.
+   */
+  recovery?: number;
 }
+
+const MILLISECONDS_IN_DAY: number = 1000 * 24 * 60 * 60;
 
 export class StopLossMarketTiming implements MarketTiming {
   public id: String;
   public threshold: number;
   public safety: number;
+  public recovery: number;
   public status: BearBull;
   private max: Candlestick;
-  private last: Candlestick;
   private count: number;
+
+  private linearRegression: OnlineLinearRegression;
 
   constructor(obj = {} as IStopLossMarketTiming) {
     let {
       id = 'STOPLOSS',
       status = BearBull.BULL,
       threshold = 95 / 100,
-      safety = 3
+      safety = 3,
+      recovery = 1
     } = obj;
     this.id = id;
     this.status = status;
     this.threshold = threshold;
     this.safety = safety;
+    this.recovery = recovery;
   }
 
   record(instant: Date, candlestick: Candlestick): void {
@@ -43,27 +64,32 @@ export class StopLossMarketTiming implements MarketTiming {
         }
         // Looks out for drops:
         if (candlestick.close < this.max.close * this.threshold) {
+          console.log("Stop Loss going BEAR", instant, candlestick, this.max)
           this.status = BearBull.BEAR;
           this.count = this.safety;
+          this.linearRegression = new OnlineLinearRegression();
         }
         break;
 
-      // Stays in bear until there have been enough green candlesticks
+      // Stays in BEAR until recovery.
       case BearBull.BEAR:
-        if (this.last) {
-          if (candlestick.close > this.last.close) {
-            this.count --;
-            if (this.count < 1) {
-              this.status = BearBull.BULL;
-              this.max = candlestick;
-            }
-          } else {
-            this.count = this.safety;
+        let x: number = instant.valueOf() / MILLISECONDS_IN_DAY;
+        this.linearRegression.regression(x , candlestick.close);
+        // Waits in bear for the duration of the safety.
+        if (this.count > 0) {
+          this.count--;
+        }
+        // Then waits until the linear regression is above recovery
+        else {
+          let a: number = this.linearRegression.getA();
+          if (a > this.recovery) {
+            console.log("Stop Loss going BULL", instant, candlestick)
+            this.status = BearBull.BULL;
+            this.max = candlestick;
           }
         }
         break;
     }
-    this.last = candlestick;
   }
 
   bearBull(): BearBull {
