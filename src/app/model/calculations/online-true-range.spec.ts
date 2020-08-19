a955546588045dbff397dddde1d418f5c8286ee6
jgonet <jgonet@pictet.com>
2020-08-19 07:47:34 +0200

    Concerns #40 - Average True Range - WIP

--- src/app/model/calculations/online-true-range.spec.ts
+++ src/app/model/calculations/online-true-range.spec.ts
@@ -1,4 +1,4 @@
-import { OnlineTrueRange } from "./online-true-range";
+import { OnlineTrueRange, OnlineAverageTrueRange } from "./online-true-range";
 import { Candlestick } from '../core/quotes';
 
 describe("OnlineTrueRange", () => {
@@ -10,21 +10,28 @@ describe("OnlineTrueRange", () => {
   it("Can detect true range when previous close is between current high-low", () => {
     let onlineTrueRange = new OnlineTrueRange();
     onlineTrueRange.trueRange(new Candlestick({close: 50}));
-    let tr = onlineTrueRange.trueRange(new Candlestick({close: 60, high: 100, low: 20}));
+    let tr = onlineTrueRange.trueRange(new Candlestick({close: 30, high: 100, low: 20}));
     expect(tr).toBe(100 - 20);
   });
 
   it("Can detect true range when previous close is above current high", () => {
     let onlineTrueRange = new OnlineTrueRange();
     onlineTrueRange.trueRange(new Candlestick({close: 50}));
-    let tr = onlineTrueRange.trueRange(new Candlestick({close: 60, high: 40, low: 20}));
+    let tr = onlineTrueRange.trueRange(new Candlestick({close: 30, high: 40, low: 20}));
     expect(tr).toBe(50 - 20);
   });
 
   it("Can detect true range when previous close is below current low", () => {
     let onlineTrueRange = new OnlineTrueRange();
     onlineTrueRange.trueRange(new Candlestick({close: 10}));
-    let tr = onlineTrueRange.trueRange(new Candlestick({close: 60, high: 40, low: 20}));
+    let tr = onlineTrueRange.trueRange(new Candlestick({close: 30, high: 40, low: 20}));
     expect(tr).toBe(40 - 10);
+  });
+});
+
+describe("OnlineAverageTrueRange", () => {
+  it("Can create a new instance", () => {
+    let onlineAverageTrueRange: OnlineAverageTrueRange = new OnlineAverageTrueRange(10);
+    expect(onlineAverageTrueRange).toBeTruthy();
   });
 });
--- src/app/model/calculations/online-true-range.ts
+++ src/app/model/calculations/online-true-range.ts
@@ -1,16 +1,41 @@
 import { Candlestick } from '../core/quotes';
 
+export class OnlineAverageTrueRange {
+  private onlineTrueRange: OnlineTrueRange;
+  private previousAtr: number;
+
+  constructor(private numberOfPeriods: number) {
+    this.onlineTrueRange = new OnlineTrueRange();
+  }
+
+  public atr(current: Candlestick): number {
+    let trueRange = this.onlineTrueRange.trueRange(current);
+    if (trueRange) {
+      let averageTrueRange: number;
+      if (this.previousAtr) {
+        averageTrueRange = (this.previousAtr * (this.numberOfPeriods - 1) + trueRange)
+            / this.numberOfPeriods;
+      } else {
+        averageTrueRange = trueRange;
+      }
+      this.previousAtr = averageTrueRange;
+      return averageTrueRange;
+    }
+    return null;
+  }
+}
+
 export class OnlineTrueRange {
   private previous: Candlestick;
 
-  public trueRange(last: Candlestick): number {
+  public trueRange(current: Candlestick): number {
     let tr: number;
     if (this.previous) {
-      let a: number = Math.max(this.previous.close, last.high);
-      let b: number = Math.min(this.previous.close, last.low);
+      let a: number = Math.max(this.previous.close, current.high);
+      let b: number = Math.min(this.previous.close, current.low);
       tr = a - b;
     }
-    this.previous = last;
+    this.previous = current;
     return tr;
   }
 }
