19268550a0962fa89b037246c74607db36bd4fcb
Jean-Michel Gonet <jmgonet@yahoo.com>
2021-03-13 07:56:17 +0100

    Simulate Ray Dalio all-weather portfolio #63
    * Added the required instruments, all from Yahoo.
    * Execute partial orders when all previous orders are settled. 
    * Fixed dividend calculation. 
    * Optional long name to make configuration easier to read.

---
+++ src/app/pages/allocations/all-weather.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-weather',
+  templateUrl: './all-weather.page.html',
+  styleUrls: ['./all-weather.page.css']
+})
+export class AllocationsAllWeatherPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
