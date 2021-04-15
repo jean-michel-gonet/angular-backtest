6ec9fe9a845dcda892264c8cb93d43b5b7711942
Jean-Michel Gonet <jmgonet@yahoo.com>
2021-04-15 19:34:26 +0200

    Concerns #20 - Split the definition of assets in multiple files

---
+++ src/assets/quotes/configuration-sp500.ts
@@ -0,0 +1,6 @@
+import {
+  NamedQuoteSource,
+  QuoteProvider,
+  ExchangeRateOperation } from '../../app/services/quotes/quote-configuration';
+
+export const SP500_SOURCES: NamedQuoteSource[] = [];
