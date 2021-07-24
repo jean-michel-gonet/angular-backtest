51f1d9af5b542ad5c5f80e05137f3287edc69323
Jean-Michel Gonet <jmgonet@yahoo.com>
2021-07-24 18:51:10 +0200

    Concerns #20 - Addind a gap indicator

---
+++ src/app/model/calculations/indicators/gap-indicator.spec.ts
@@ -0,0 +1,60 @@
+import { Periodicity } from '../../core/period';
+import { Candlestick } from '../../core/quotes';
+import { ConfigurableSource, ConfigurablePreprocessing } from './configurable-source';
+import { GapIndicator, GapIndicatorConfigurationMaximumGapWidthError } from "./gap-indicator";
+
+describe('GapIndicator', () => {
+  it('Can create a new instance', () => {
+    expect(new GapIndicator({
+      numberOfPeriods: 20,
+      periodicity: Periodicity.MONTHLY,
+      source: ConfigurableSource.HIGH,
+      preprocessing: ConfigurablePreprocessing.TYPICAL,
+      maximumGapWidth: 2
+    }))
+    .toBeTruthy();
+  });
+
+  it('Can raise an exception if maximumGapWidth is larger than numberOfPeriods', () => {
+    expect(() => {
+      new GapIndicator({
+        numberOfPeriods: 2,
+        periodicity: Periodicity.MONTHLY,
+        maximumGapWidth: 20
+      });
+    }).toThrow(new GapIndicatorConfigurationMaximumGapWidthError(2, 20))
+  });
+
+  it('Can calculate gap of only one value, and is always zero', () => {
+    let gap = new GapIndicator({numberOfPeriods: 13, periodicity: Periodicity.DAILY});
+    expect(gap.calculate(new Date(2019, 7 - 1, 19), new Candlestick({close: 30.00}))).toBeCloseTo(0, 2);
+  });
+
+  it('Can calculate gap over 5 days', () => {
+    let gap = new GapIndicator({numberOfPeriods: 5, periodicity: Periodicity.DAILY});
+    gap.calculate(new Date(2019, 7 - 1,   9), new Candlestick({close: 46.00}));
+    gap.calculate(new Date(2019, 7 - 1,  10), new Candlestick({close: 47.00}));
+    gap.calculate(new Date(2019, 7 - 1,  11), new Candlestick({close: 48.00}));
+    gap.calculate(new Date(2019, 7 - 1,  12), new Candlestick({close: 49.00}));
+
+    let maximumGap = gap.calculate(new Date(2019, 7 - 1,  13), new Candlestick({close: 50.00}));
+    expect(maximumGap).toBeCloseTo(8.7, 2);
+  });
+
+  it('Can calculate gap over 10 days', () => {
+    let gap = new GapIndicator({numberOfPeriods: 10, periodicity: Periodicity.DAILY});
+    gap.calculate(new Date(2019, 7 - 1,   3), new Candlestick({close: 40.00}));
+    gap.calculate(new Date(2019, 7 - 1,   4), new Candlestick({close: 41.00}));
+    gap.calculate(new Date(2019, 7 - 1,   5), new Candlestick({close: 42.00}));
+    gap.calculate(new Date(2019, 7 - 1,   6), new Candlestick({close: 43.00}));
+    gap.calculate(new Date(2019, 7 - 1,   7), new Candlestick({close: 44.00}));
+    gap.calculate(new Date(2019, 7 - 1,   8), new Candlestick({close: 45.00}));
+    gap.calculate(new Date(2019, 7 - 1,   9), new Candlestick({close: 46.00}));
+    gap.calculate(new Date(2019, 7 - 1,  10), new Candlestick({close: 47.00}));
+    gap.calculate(new Date(2019, 7 - 1,  11), new Candlestick({close: 48.00}));
+    gap.calculate(new Date(2019, 7 - 1,  12), new Candlestick({close: 49.00}));
+
+    let maximumGap = gap.calculate(new Date(2019, 7 - 1,  13), new Candlestick({close: 50.00}));
+    expect(maximumGap).toBeCloseTo(25, 2);
+  });
+});
---
+++ src/app/model/calculations/indicators/gap-indicator.ts
@@ -0,0 +1,93 @@
+import { IndicatorConfiguration, IndicatorConfigurationError } from './configurable-source';
+import { ConfigurableSourceIndicator } from './configurable-source-indicator';
+
+export interface GapIndicatorConfiguration extends IndicatorConfiguration {
+  /** Maximum number of periods considered when measuging a gap. **/
+  maximumGapWidth?: number;
+}
+
+export class GapIndicatorConfigurationMaximumGapWidthError extends IndicatorConfigurationError {
+  constructor(public numberOfPeriods: number, public maximumGapWidth: number) {
+    super("Gap Indicator numberOfPeriods smaller than maximumGapWidth: "
+      + numberOfPeriods + " < " + maximumGapWidth);
+  }
+}
+
+interface Gap {
+  value: number;
+  gap: number;
+  instant?: Date;
+  gapWith?: Gap;
+}
+
+/**
+ * Looks for the maximum gap between any two values within the relevant period
+ * range.
+ * Gaps are calculated over a maximum of {#maximumGapWidth} number of periods.
+ */
+export class GapIndicator extends ConfigurableSourceIndicator {
+  public maximumGapWidth: number;
+  private positionCounter: number = 0;
+  private gaps: Gap[];
+  private maximumGap: Gap;
+
+  /**
+   * Class constructor.
+   * @param obj The configuration.
+   */
+  constructor(obj = {} as GapIndicatorConfiguration) {
+    super(obj);
+    let {
+      maximumGapWidth = this.numberOfPeriods
+    } = obj;
+    if (this.numberOfPeriods < maximumGapWidth) {
+      throw new GapIndicatorConfigurationMaximumGapWidthError(this.numberOfPeriods, maximumGapWidth);
+    } else {
+      this.maximumGapWidth = maximumGapWidth;
+    }
+    this.gaps = [];
+  }
+
+  protected compute(instant: Date, value: number): number {
+    let incomingGap = this.calculateIncomingGap(instant, value);
+    this.gaps.push(incomingGap);
+    if (!this.maximumGap || this.maximumGap.gap < incomingGap.gap) {
+      this.maximumGap = incomingGap;
+    }
+
+    if (this.gaps.length > this.numberOfPeriods) {
+      let outgoingGap = this.gaps.shift();
+      if (outgoingGap == this.maximumGap) {
+        this.maximumGap == null;
+        this.gaps.forEach(gap => {
+          if (!this.maximumGap || this.maximumGap.gap < gap.gap) {
+            this.maximumGap = gap;
+          }
+        });
+      }
+    }
+
+    return this.maximumGap.gap;
+  }
+
+  private calculateIncomingGap(instant: Date, value: number): Gap {
+    let incomingGap: Gap = {
+      instant: instant,
+      value: value,
+      gap: 0
+    };
+
+    let from = Math.max(0, this.gaps.length - this.maximumGapWidth);
+    let to = this.gaps.length - 1;
+    for(var i = from; i <= to; i++) {
+      let previousValue:number = this.gaps[i].value;
+      let gap = 100 * (value - previousValue) / previousValue;
+      if (gap > incomingGap.gap) {
+        incomingGap.gap = gap;
+        incomingGap.gapWith = this.gaps[i];
+      }
+    }
+
+    return incomingGap;
+  }
+}
