39f267c7418e82553ee6f1eee71a04788ad0e372
Jean-Michel Gonet <jmgonet@yahoo.com>
2021-08-27 16:29:13 +0200

    Concerns #20 - Period can detect day of the week

--- src/app/model/core/period.spec.ts
+++ src/app/model/core/period.spec.ts
@@ -54,6 +54,50 @@ describe('Period', () => {
     expect(period.changeOfPeriod(new Date(2002,  1,  1))).toBe(false);
   });
 
+  it('Can detect every Wednesday of the week', () => {
+    let period: Period = new Period(Periodicity.WEEKLY, 3);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  2))).withContext(" 2 August (first)").toBe(true);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  3))).withContext(" 3 August (Tue.)").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  4))).withContext(" 4 August (Wed.)").toBe(true);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  5))).withContext(" 5 August (Thu.)").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  6))).withContext(" 6 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  7))).withContext(" 7 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  8))).withContext(" 8 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  9))).withContext(" 9 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 10))).withContext("10 August (Tue.)").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 11))).withContext("11 August (Wed.)").toBe(true);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 12))).withContext("12 August").toBe(false);
+  });
+
+  it('Can detect every other Wednesday of the week', () => {
+    let period: Period = new Period(Periodicity.WEEKLY, 3, 2);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  2))).withContext(" 2 August (first)").toBe(true);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  3))).withContext(" 3 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  4))).withContext(" 4 August (Wed.)").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  5))).withContext(" 5 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  6))).withContext(" 6 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  7))).withContext(" 7 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  8))).withContext(" 8 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1,  9))).withContext(" 9 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 10))).withContext("10 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 11))).withContext("11 August (Wed.)").toBe(true);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 12))).withContext("12 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 13))).withContext("13 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 14))).withContext("14 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 15))).withContext("15 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 16))).withContext("16 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 17))).withContext("17 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 18))).withContext("18 August (Wed.)").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 19))).withContext("19 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 20))).withContext("20 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 21))).withContext("21 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 23))).withContext("23 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 24))).withContext("24 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 25))).withContext("25 August (Wed.)").toBe(true);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 26))).withContext("26 August").toBe(false);
+    expect(period.changeOfPeriod(new Date(2021, 8 - 1, 27))).withContext("27 August").toBe(false);
+  });
+
   it('Can wait for 25 days', () => {
     let period: Period = new Period(Periodicity.DAILY);
     expect(period.timeIsUp(new Date(2020, 10 - 1,  1), 25)).toBe(false);
